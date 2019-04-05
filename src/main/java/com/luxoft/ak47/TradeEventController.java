d820f11d6ce1655d2732eb8f1c5cc113af7acb74
dariaKra <d.i.kravchuk1@gmail.com>
2019-04-05 15:09:36 +0200

    new project

---
+++ src/main/java/com/luxoft/ak47/TradeEventController.java
@@ -0,0 +1,19 @@
+package com.luxoft.ak47;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+@RestController
+public class TradeEventController {
+
+
+    @RequestMapping(name ="/tradeEvent", produces = MediaType.TEXT_XML_VALUE )
+
+    String tradeEvent() {
+        return "<version><version>0</version></version>";
+
+    }
+}
